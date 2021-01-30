c07a8faedc2966c0f656a8c6c83e7ecfab7d3e7a
larangogon <67110144+larangogon@users.noreply.github.com>
2021-01-29 23:18:49 -0500

    feat: productos similares

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -38,17 +38,7 @@
             </div>
             <div class="grid grid-cols-2 gap-4 my-8">
                 @foreach($product->similar() as $product)
-                    <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
-                        <a href="{{route('show', $product->slug)}}">
-                            <img src="{{ $product->image }}" class="rounded-md mb-2">
-                            <h2 class="text-lg text-gray-600 truncate uppercase">{{$product->name}}</h2>
-                            <h3 class="text-lg text-gray-500">{{$product->excerpt}}</h3>
-
-                            <img
-                                src="{{$product->user->avatar}}" class="rounded-full mx-auto h-16 w-16"
-                            >
-                        </a>
-                    </div>
+                    <x-card-product :product="$product"></x-card-product>
                 @endforeach
             </div>
 
